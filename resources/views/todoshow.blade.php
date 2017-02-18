5a9dfe99af79df879731b1992fc5e0a2dd66808d
spell1612 <spell1612@gmail.com>
2017-02-18 14:25:28 +0530

    impro1

--- resources/views/todoshow.blade.php
+++ resources/views/todoshow.blade.php
@@ -4,11 +4,18 @@
         <h2>To-Do View</h2>
     </head>
     <body>
-       
+       <table border=1>
+       <th>ID</th>
+       <th>Tag</th>
+       <th>About</th>
         @foreach($asdf as $a)
-            {{ $a->id." ".$a->task." ".$a->description }}<br>
+            <tr>
+            <td>{{$a->id}}</td>
+            <td>{{$a->task}}</td>
+            <td>{{$a->description}}</td>
+            </tr>
         @endforeach
-    
+       </table>
     </body>
 
 </html>
--- resources/views/todoweb.blade.php
+++ resources/views/todoweb.blade.php
@@ -6,8 +6,8 @@
     <body>
         <form method=post action='/dbsave'>
             Task <input type="text" name='stuff'><br>
-            Description<textarea type="text" name='texty'></textarea><br>
-            <input type="submit" value='Button'><br>
+            Description<textarea name='texty'></textarea><br>
+            <input type="submit" value='Save'><br>
             {{csrf_field()}}
         <form>
         <a href ='/dbshow'><input type=button value=View></a><br>
