83a47848a58756e1918e8c27d76308e4bc279d73
spell1612 <spell1612@gmail.com>
2017-02-18 10:27:04 +0530

    never worked so hard b4

---
+++ database/migrations/2017_02_18_044249_todomig.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Todomig extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+         Schema::create('todotables', function (Blueprint $table) {
+            $table->increments('id')->index();
+            $table->string('task')->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::drop('todotables');
+    }
+}
---
+++ resources/views/todoweb.blade.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>todo</title>
+        To-Do
+    </head>
+    <body>
+        <form>
+            <input type="text"><br>
+            <input type="submit"><br>
+        <form>
+    </body>
+</html>
